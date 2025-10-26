028739db4cf39331e015d6179eff14ab894ef8ea
ITALO O RESENDE <italoliveira5@gmail.com>
2025-10-26 14:41:41 -0300

    documento_sem_situacao

--- index.php
+++ index.php
@@ -131,9 +131,6 @@ $result = $stmt->get_result();
     <link rel="stylesheet" href="Style.css">
 </head>
 <body>
-    <a href="gerarDocumento.php?teste=true" class="btn btn-warning" style="margin-left: 10px;">
-    TESTE API DOCS
-</a>
     <div class="container">
         <header class="main-header">
             <h1>Grupo Vitória da União (GVU)</h1>
